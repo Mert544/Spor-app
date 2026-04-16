58f1f57b0742fa187ce1e2c7d2a4c7421ebee832
Mert544 <mertelgul@gmail.com>
2026-04-16 23:16:19 +0300

    feat: Refactor long file: C:\Users\salio\OneDrive\Desktop\KARIŞIK\SPOR UYGULAMA\src\components\Coach\CoachPage.jsx

--- src/components/Coach/CoachPage.jsx
+++ src/components/Coach/CoachPage.jsx
@@ -1,2 +1,2 @@
-// [LLM-template] Task: TASK-0026 | Generated at: 2026-04-16T23:16:19.136369
+// [LLM-template] Task: TASK-0026 | Generated at: 2026-04-16T23:16:19.440170
 // Description: File has 798 lines. Split into smaller modules.
